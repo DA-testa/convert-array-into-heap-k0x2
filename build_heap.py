db44451ddff2b75fbd1fe57e8f0dd85c295e22bd
k0x2 <125827996+k0x2@users.noreply.github.com>
2023-04-11 20:12:36 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,44 @@
-# python3
+# KarlisOlmanis
 
 
 def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    mainja = []
+    lielums = len(data)
+    for i in range(lielums // 2, -1, -1):
+        maz(data, i, mainja)
+    return mainja
 
-
-    return swaps
-
-
+def maz(data, i, mainja):
+    lielums = len(data)
+    min_inde = i
+    kreisais = 2 * i + 1
+    labais = 2 * i + 2
+    if kreisais < lielums and data[kreisais] < data[min_inde]:
+        min_inde = kreisais
+    if labais < lielums and data[labais] < data[min_inde]:
+        min_inde = labais
+    if min_inde != i:
+        mainja.append((i, min_inde))
+        data[i], data[min_inde] = data[min_inde], data[i]
+        maz(data, min_inde, mainja)
+        
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
+    ievade = input()
+    if "I" in ievade:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+    elif "F" in ievade:
+        fails = input()
+        atrasanas = './tests/'
+        faila_vieta = os.path.join(atrasanas, fails)
+        with open(faila_vieta, mode="r") as file:
+            n = int(file.readline())
+            data = list(map(int, file.readline().split()))
+    mainja = build_heap(data)
+    print(len(mainja))
+    for i, j in mainja:
         print(i, j)
-
-
+        
 if __name__ == "__main__":
     main()
